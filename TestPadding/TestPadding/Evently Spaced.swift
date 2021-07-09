e0a123397d9953bcaf48153bba9f89303cfd4a8d
RecherJ <ygliusz@foxmail.com>
2021-07-09 13:54:08 +0800

    update Evently Spaced

--- TestPadding/TestPadding/Evently Spaced.swift
+++ TestPadding/TestPadding/Evently Spaced.swift
@@ -19,7 +19,60 @@ struct Evently_Spaced: View {
                 .font(.title)
                 .padding()
                 .background(Color.yellow)
-            
+
+            Text("Before")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Names")
+                        .font(.largeTitle)
+                        .underline()
+                    Text("Chase")
+                    Text("Rodrigo")
+                    Text("Mark")
+                    Text("Evans")
+                }
+                VStack(alignment: .leading) {
+                    Text("Color")
+                        .font(.largeTitle)
+                        .underline()
+                    Text("Red")
+                    Text("Orange")
+                    Text("Green")
+                    Text("Blue")
+                }
+            }
+
+            Text("After")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.yellow)
+
+            HStack {
+                Spacer()
+
+                VStack(alignment: .leading) {
+                    Text("Names")
+                        .font(.largeTitle)
+                        .underline()
+                    Text("Chase")
+                    Text("Rodrigo")
+                    Text("Mark")
+                    Text("Evans")
+                }
+
+                Spacer()
+
+                VStack(alignment: .leading) {
+                    Text("Color")
+                        .font(.largeTitle)
+                        .underline()
+                    Text("Red")
+                    Text("Orange")
+                    Text("Green")
+                    Text("Blue")
+                }
+                Spacer()
+            }
 
         }
         .font(.largeTitle)
