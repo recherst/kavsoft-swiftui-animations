9ce361cb1258cbd496fb039458ecac37226c0305
RecherJ <ygliusz@foxmail.com>
2021-07-09 23:37:45 +0800

    safe area insets

---
+++ TestPadding/TestPadding/Safe Area Insets.swift
@@ -0,0 +1,41 @@
+//
+//  Safe Area Insets.swift
+//  TestPadding
+//
+//  Created by 刘勇刚 on 2021/7/9.
+//
+
+import SwiftUI
+
+struct Safe_Area_Insets: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("GeometryReader")
+            Text("SafeAreaInsets")
+                .foregroundColor(.gray)
+                .font(.title)
+            Text("GeometryReader can also tell you the safe area insets it has.")
+
+            GeometryReader { geometry in
+                VStack {
+                    Text("geometry.safeAreaInsets.leading: \(geometry.safeAreaInsets.leading)")
+                    Text("geometry.safeAreaInsets.trailing: \(geometry.safeAreaInsets.trailing)")
+                    Text("geometry.safeAreaInsets.top: \(geometry.safeAreaInsets.top)")
+                    Text("geometry.safeAreaInsets.bottom: \(geometry.safeAreaInsets.bottom)")
+                }
+            }
+            .font(.title)
+            .background(Color.pink)
+            .foregroundColor(.white)
+        }
+        .font(.largeTitle)
+    }
+}
+
+struct Safe_Area_Insets_Previews: PreviewProvider {
+    static var previews: some View {
+        Safe_Area_Insets()
+            .previewLayout(PreviewLayout.fixed(width: 844, height: 390))
+            .environment(\.horizontalSizeClass, .compact)
+    }
+}
