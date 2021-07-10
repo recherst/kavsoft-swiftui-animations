803cbad234560327300146553b8cad89ed91b1d5
RecherJ <ygliusz@foxmail.com>
2021-07-10 11:07:00 +0800

    floating action button

---
+++ TestPadding/TestPadding/Button with Images.swift
@@ -0,0 +1,37 @@
+//
+//  Button with Images.swift
+//  TestPadding
+//
+//  Created by 刘勇刚 on 2021/7/10.
+//
+
+import SwiftUI
+
+struct Button_with_Images: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Button")
+                .font(.largeTitle)
+            Text("With Images")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("Buttons work fine with the SF Symbol. But what if you wanted to used a photo? Look what happens:")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.title)
+                .foregroundColor(Color.purple)
+
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Image("yosemite")
+                    .resizable()
+                    .cornerRadius(40)
+            }
+        }
+    }
+}
+
+struct Button_with_Images_Previews: PreviewProvider {
+    static var previews: some View {
+        Button_with_Images()
+    }
+}
---
+++ TestPadding/TestPadding/FloatingActionButton.swift
@@ -0,0 +1,57 @@
+//
+//  FloatingActionButton.swift
+//  TestPadding
+//
+//  Created by 刘勇刚 on 2021/7/10.
+//
+
+import SwiftUI
+
+struct FloatingActionButton: View {
+    var body: some View {
+        ZStack {
+            VStack(spacing: 40) {
+                Text("Button")
+                    .font(.largeTitle)
+                Text("Floating")
+                    .font(.title)
+                    .foregroundColor(.gray)
+
+                Text("You can also create floating buttons by using a ZStack so the button is on the top layer, over everything else")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .font(.title)
+                    .background(Color.purple)
+
+                Spacer()
+            }
+
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "plus")
+                            .font(.title)
+                    }
+                    .padding()
+                    .foregroundColor(Color.white)
+                    .background(Color.purple)
+                    .clipShape(Capsule())
+                }
+                // Add 30 points on the trailing side of the button
+                .padding(.trailing, 30)
+            }
+        }
+    }
+}
+
+// See the section on the Overlay modifier in the Layout Modifiers chapter for more
+// ways to accomplish the same thing.
+
+struct FloatingActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FloatingActionButton()
+            .preferredColorScheme(.dark)
+    }
+}
