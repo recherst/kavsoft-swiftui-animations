61e5e3bc6624bd84a0ceddb7ef073fbbbab42c47
RecherJ <ygliusz@foxmail.com>
2021-07-10 11:35:35 +0800

    DatePicker titles

---
+++ TestPadding/TestPadding/DatePickerTitles.swift
@@ -0,0 +1,44 @@
+//
+//  DatePickerTitles.swift
+//  TestPadding
+//
+//  Created by 刘勇刚 on 2021/7/10.
+//
+
+import SwiftUI
+
+struct DatePickerTitles: View {
+
+    @State private var nextFullMoonDate = Date()
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("DatePicker")
+                .font(.largeTitle)
+            Text("Titles")
+                .foregroundColor(.gray)
+                .font(.title)
+            Text("You can privide an optional title for pickers.")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.yellow)
+                .font(.title)
+
+            // The first parameter is a label or title for the picker
+            DatePicker("Date of next full moon", selection: $nextFullMoonDate, displayedComponents: .date)
+                .padding(.horizontal)
+
+            Text("This doesn't really look good. So you might want to add your own title.")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.yellow)
+                .font(.title)
+        }
+    }
+}
+
+struct DatePickerTitles_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickerTitles()
+    }
+}
