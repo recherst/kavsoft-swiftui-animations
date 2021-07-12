2d58dd5b94e81de735d75ab373b77417ed6f177c
RecherJ <ygliusz@foxmail.com>
2021-07-12 19:52:22 +0800

    list navigation with no navigation view

--- TestPadding/TestPadding/TestPaddingApp.swift
+++ TestPadding/TestPadding/TestPaddingApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct TestPaddingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            NavigationWithListNoNavBar()
         }
     }
 }
