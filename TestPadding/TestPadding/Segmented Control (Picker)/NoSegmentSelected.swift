82873cee8ad97ab9176b015f9d1b958032ed7f1b
RecherJ <ygliusz@foxmail.com>
2021-07-13 13:46:09 +0800

    no segment selected

---
+++ TestPadding/TestPadding/Segmented Control (Picker)/NoSegmentSelected.swift
@@ -0,0 +1,61 @@
+//
+//  NoSegmentSelected.swift
+//  TestPadding
+//
+//  Created by 刘勇刚 on 2021/7/13.
+//
+
+import SwiftUI
+
+struct NoSegmentSelected: View {
+    @State private var selection = 0
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Segmented Control (Picker)")
+                .font(.largeTitle)
+            Text("No Segment Selected")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("This segmented control will have nothing selected because the default state variable does not match any of the segment tag values.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.title)
+                .background(Color.red)
+                .foregroundColor(Color.white)
+
+            Text("How many meals do you eat?")
+                .foregroundColor(.gray)
+                .font(.title)
+
+            Picker("", selection: $selection) {
+                Text("One")
+                    .tag(1)
+                Text("Two")
+                    .tag(2)
+                Text("Three")
+                    .tag(3)
+                Text("More")
+                    .tag(4)
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.red, lineWidth: selection == 0 ? 1 : 0)
+            )
+            .padding()
+
+            Text("The red outline will go away once a selection is made.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.title)
+                .background(Color.red)
+                .foregroundColor(Color.white)
+        }
+    }
+}
+
+struct NoSegmentSelected_Previews: PreviewProvider {
+    static var previews: some View {
+        NoSegmentSelected()
+    }
+}
