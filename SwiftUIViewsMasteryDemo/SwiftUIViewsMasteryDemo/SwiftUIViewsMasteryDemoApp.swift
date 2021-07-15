93e25811f34a3aeb77cf1d2cbc506c74ed75e6b0
RecherJ <ygliusz@foxmail.com>
2021-07-15 15:36:59 +0800

    circular shape trim solid shape

--- SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemoApp.swift
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemoApp.swift
@@ -12,7 +12,7 @@ struct SwiftUIViewsMasteryDemoApp: App {
     var body: some Scene {
         WindowGroup {
 //            ContentView()
-            CircularShapeTrimOutline()
+            TrimSolidShape()
         }
     }
 }
