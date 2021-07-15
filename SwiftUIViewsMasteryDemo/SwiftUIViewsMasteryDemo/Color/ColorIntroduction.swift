8017d16e7c21be17b8e223638db6b3cf1f77f1e2
RecherJ <ygliusz@foxmail.com>
2021-07-15 15:55:42 +0800

    color introduction

---
+++ SwiftUIViewsMasteryDemo/SwiftUIViewsMasteryDemo/Color/ColorIntroduction.swift
@@ -0,0 +1,44 @@
+//
+//  ColorIntroduction.swift
+//  SwiftUIViewsMasteryDemo
+//
+//  Created by 刘勇刚 on 2021/7/15.
+//
+
+import SwiftUI
+
+struct ColorIntroduction: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            Text("Color")
+                .font(.largeTitle)
+            Text("Colors are Views")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("You can treat colors as views with their own frames and modifiers")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.yellow)
+                .layoutPriority(1)
+
+            HStack(spacing: 40) {
+                Color.pink
+                    .frame(width: 88, height: 88)
+                Color.blue
+                    .frame(width: 88, height: 88)
+                Color.purple
+                    .frame(width: 88, height: 88)
+                    .cornerRadius(20)
+            }
+            Spacer()
+        }
+        .font(.title)
+    }
+}
+
+struct ColorIntroduction_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorIntroduction()
+            .preferredColorScheme(.dark)
+    }
+}
