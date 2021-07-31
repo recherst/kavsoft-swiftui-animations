7d3928ae7ed29f3cabc335207576fff4a3a7b95a
recherst <ygliusz@gmail.com>
2021-07-31 17:26:14 +0800

    Create custom carousel slider project

---
+++ CustomCarouselSlider/CustomCarouselSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomCarouselSlider
+//
+//  Created by recherst on 2021/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCarouselSlider/CustomCarouselSlider/CustomCarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCarouselSliderApp.swift
+//  CustomCarouselSlider
+//
+//  Created by recherst on 2021/7/31.
+//
+
+import SwiftUI
+
+@main
+struct CustomCarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
