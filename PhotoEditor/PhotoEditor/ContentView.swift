288143fe02dc39c4945de6f5778bba1f2d1c5845
recherst <ygliusz@gmail.com>
2021-08-01 12:23:01 +0800

    Initialize project

---
+++ PhotoEditor/PhotoEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoEditor
+//
+//  Created by 刘勇刚 on 2021/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoEditor/PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by 刘勇刚 on 2021/8/1.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
