115f42615b370b6d09fd12138ce4f53efa79f759
recherst <ygliusz@gmail.com>
2021-08-06 23:45:58 +0800

    Complete genres and liked songs content

--- SpotifyUI/SpotifyUI/View/Home.swift
+++ SpotifyUI/SpotifyUI/View/Home.swift
@@ -49,50 +49,99 @@ struct Home: View {
                     // Carousel list
                     TabView {
                         ForEach(recentlyPlayed) { item in
-                            ZStack(alignment: .bottomLeading) {
-                                Image(item.albumCover)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .cornerRadius(20)
-                                // Dark shading at button so that the data will be visible
-                                    .overlay(
-                                        LinearGradient(
-                                            gradient: Gradient(colors: [Color.clear, Color.clear,  Color.black]),
-                                            startPoint: .top,
-                                            endPoint: .bottom)
-                                            .cornerRadius(20)
-                                    )
+                            GeometryReader { geometry in
+                                ZStack(alignment: .bottomLeading) {
+                                    Image(item.albumCover)
+                                        .resizable()
+                                        // If you're using fill then you must specify the width
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: geometry.frame(in: .global).width)
+                                        .cornerRadius(20)
+                                    // Dark shading at button so that the data will be visible
+                                        .overlay(
+                                            LinearGradient(
+                                                gradient: Gradient(colors: [Color.clear, Color.clear,  Color.black]),
+                                                startPoint: .top,
+                                                endPoint: .bottom)
+                                                .cornerRadius(20)
+                                        )
 
-                                HStack(spacing: 15) {
-                                    Button(action: {}, label: {
-                                        Image(systemName: "play.fill")
-                                            .font(.title)
-                                            .foregroundColor(.white)
-                                            .padding(20)
-                                            .background(Color("logoColor"))
-                                            .clipShape(Circle())
+                                    HStack(spacing: 15) {
+                                        Button(action: {}, label: {
+                                            Image(systemName: "play.fill")
+                                                .font(.title)
+                                                .foregroundColor(.white)
+                                                .padding(20)
+                                                .background(Color("logoColor"))
+                                                .clipShape(Circle())
 
-                                    })
+                                        })
 
-                                    VStack(alignment: .leading, content: {
-                                        Text(item.albumName)
-                                            .font(.title2)
-                                            .fontWeight(.heavy)
-                                            .foregroundColor(.white)
+                                        VStack(alignment: .leading, content: {
+                                            Text(item.albumName)
+                                                .font(.title2)
+                                                .fontWeight(.heavy)
+                                                .foregroundColor(.white)
 
-                                        Text(item.albumAuthor)
-                                            .font(.none)
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.white)
-                                    })
+                                            Text(item.albumAuthor)
+                                                .font(.none)
+                                                .fontWeight(.bold)
+                                                .foregroundColor(.white)
+                                        })
+                                    }
+                                    .padding()
                                 }
-                                .padding()
                             }
                             .padding(.horizontal)
+                            .frame(height: 350)
                         }
                     }
                     .frame(height: 350)
                     .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .padding(.top, 20)
+
+                    Text("Genres")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 30)
+
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 20, content: {
+                        // List of genres
+                        ForEach(generes, id: \.self) { genre in
+                            Text(genre)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 8)
+                                .frame(maxWidth: .infinity)
+                                .background(Color.white.opacity(0.06))
+                                .clipShape(Capsule())
+                        }
+                    })
+                    .padding(.top, 20)
+
+                    Text("Liked Songs")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 30)
+
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 2), spacing: 10, content: {
+                        // Liked songs
+                        ForEach(likeSongs.indices, id: \.self) { index in
+                            GeometryReader { geometry in
+                                Image(likeSongs[index].albumCover)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: geometry.frame(in: .global).width, height: 150)
+                                    .clipShape(CustomCorners(corners: index % 2 == 0 ? [.topLeft, .bottomLeft] : [.topRight, .bottomRight], radius:  15))
+                            }
+                            .frame(height: 150)
+                        }
+                    })
+                    .padding(.horizontal)
                     .padding(.top, 20)
                 }
                 .padding()
@@ -106,5 +155,16 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+
+// Custom corner for single side corner image
+struct CustomCorners: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
