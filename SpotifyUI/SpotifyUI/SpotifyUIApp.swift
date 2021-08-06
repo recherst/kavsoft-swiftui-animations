0fa5d1bf123091b1fd63b86a8a8501198adcd7be
recherst <ygliusz@gmail.com>
2021-08-06 21:30:46 +0800

    Initialize project and add image assets

---
+++ SpotifyUI/SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by recherst on 2021/8/6.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
