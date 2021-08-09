3d17c89fb299420bb2548c2b72d258df2aa636d9
recherst <ygliusz@gmail.com>
2021-08-09 21:05:59 +0800

    Add images in assets

---
+++ FoodAppUI/FoodAppUI/FoodAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppUIApp.swift
+//  FoodAppUI
+//
+//  Created by recherst on 2021/8/9.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
