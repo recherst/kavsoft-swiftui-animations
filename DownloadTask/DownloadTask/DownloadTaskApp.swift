2229cdb076f99aa44b03a7c4770540ae230f0644
recherst <ygliusz@gmail.com>
2021-08-10 08:33:31 +0800

    Add download task project

---
+++ DownloadTask/DownloadTask/DownloadTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadTaskApp.swift
+//  DownloadTask
+//
+//  Created by recherst on 2021/8/10.
+//
+
+import SwiftUI
+
+@main
+struct DownloadTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
