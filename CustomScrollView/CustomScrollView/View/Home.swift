2bf727ee2f0f7a699bce5197b9c6a9d00f6c511c
recherst <ygliusz@gmail.com>
2021-08-16 23:04:09 +0800

    Offset the title when scrollview scrolls

--- CustomScrollView/CustomScrollView/View/Home.swift
+++ CustomScrollView/CustomScrollView/View/Home.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct Home: View {
     @State var searchQuery = ""
+    @State var offset: CGFloat = 0
+    // Start offset
+    @State var startOffset: CGFloat = 0
+
+    // To move title to center were getting the title width
+    @State var titleOffset: CGFloat = 0
+
     var body: some View {
         VStack {
             VStack {
@@ -39,10 +46,25 @@ struct Home: View {
                             .foregroundColor(.gray)
                     )
                     .font(.largeTitle)
+                    .overlay(
+                        GeometryReader { geometry -> Color in
+                            let width = geometry.frame(in: .global).maxX
+                            DispatchQueue.main.async {
+                                // Store
+                                if titleOffset == 0 {
+                                    titleOffset = width
+                                }
+                            }
+                            return Color.clear
+                        }
+                        .frame(width: 0, height: 0)
+                    )
+                    .padding()
+                    // Getting offset and moving the view
+                    .offset(getOffset)
 
                     Spacer()
                 }
-                .padding()
 
                 // Search bar
                 HStack(spacing: 15) {
@@ -72,14 +94,42 @@ struct Home: View {
                 .padding()
             }
 
-            ScrollView(showsIndicators: false, content: {
+            ScrollView(.vertical, showsIndicators: false, content: {
                 VStack(spacing: 15) {
                     ForEach(friends) { friend in
                         FriendRowView(friend: friend)
                     }
                 }
             })
+            .padding(.top, 10)
+            // Get offset by using geometry reader
+            .overlay(
+                GeometryReader { geometry -> Color in
+                    let minY = geometry.frame(in: .global).minY
+                    DispatchQueue.main.async {
+                        // To get original offset
+                        // ie from 0
+                        // just minus start offset
+                        if startOffset == 0 {
+                            startOffset = minY
+                        }
+                        offset = startOffset - minY
+                        print(offset)
+                    }
+                    return Color.clear
+                }
+                .frame(width: 0, height: 0)
+
+                ,alignment: .top
+            )
         }
+    }
+
+    var getOffset: CGSize {
+        var size: CGSize = .zero
+        size.width = offset
+        size.height = -offset
+        return size
     }
 }
 
