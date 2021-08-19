dc30b16b1c3049650b04f952b761d38c108751a9
recherst <ygliusz@gmail.com>
2021-08-19 21:57:00 +0800

    Add drag-gesture and curve path

--- LiquidSwipe/LiquidSwipe/Home.swift
+++ LiquidSwipe/LiquidSwipe/Home.swift
@@ -8,19 +8,55 @@
 import SwiftUI
 
 struct Home: View {
+    // Liquid swipe offset
+    @State var offset: CGSize = .zero
+
     var body: some View {
         ZStack {
             Color("bg")
-                .clipShape(LiquidSwipe())
+                // Give content before clipping
+                .overlay(
+                    // Content
+                    VStack(alignment: .leading, spacing: 30, content: {
+                        Text("From Gamers")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                        Text("Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                    })
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 30)
+                    .offset(x: -15)
+                )
+                .clipShape(LiquidSwipe(offset: offset))
                 .ignoresSafeArea()
                 // arrow
                 .overlay(
                     Image(systemName: "chevron.left")
                         .font(.largeTitle)
+                        // For drag-gesture to identify
+                        .frame(width: 50, height: 50)
+                        .contentShape(Rectangle())
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ value in
+                                    // Aniate swipe offset
+                                    withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.6, blendDuration: 0.6)) {
+                                        offset = value.translation
+                                    }
+                                })
+                                .onEnded({ value in
+                                    withAnimation(.spring()) {
+                                        offset = .zero
+                                    }
+                                })
+                        )
                         .offset(y: 65)
 
                     , alignment: .topTrailing
                 )
+
                 .padding(.trailing)
         }
     }
@@ -34,9 +70,15 @@ struct Home_Previews: PreviewProvider {
 
 // Custom Shape
 struct LiquidSwipe: Shape {
+    // Get offset value
+    var offset: CGSize
     func path(in rect: CGRect) -> Path {
         return Path { path in
-            let width = rect.width
+            // When user moves left
+            // Increase size both in top and bottom
+            // So it will create a liquid swipe effect
+
+            let width = rect.width + (-offset.width > 0 ? offset.width : 0)
             // First constructing rectangle shape
             path.move(to: CGPoint(x: 0, y: 0))
             path.addLine(to: CGPoint(x: rect.width, y: 0))
@@ -44,10 +86,22 @@ struct LiquidSwipe: Shape {
             path.addLine(to: CGPoint(x: 0, y: rect.height))
 
             // Now constructing curve shape
-            path.move(to: CGPoint(x: width, y: 80))
+            // From
+            let from = 80 + offset.width
+            path.move(to: CGPoint(x: rect.width, y: from > 80 ? 80 : from))
+
+            // Also add height
+            var to = 180 + (offset.height) + (-offset.width)
+            to = to < 180 ? 180 : to
+
             // Mid between 80 - 180
-            let mid: CGFloat = 80 + ((180 - 80) / 2)
-            path.addCurve(to: CGPoint(x: width, y: 180), control1: CGPoint(x: width - 50, y: mid), control2: CGPoint(x: width - 50, y: mid))
+            let mid: CGFloat = 80 + ((to - 80) / 2)
+
+            path.addCurve(
+                to: CGPoint(x: rect.width, y: to),
+                control1: CGPoint(x: width - 50, y: mid),
+                control2: CGPoint(x: width - 50, y: mid)
+            )
         }
     }
 }
