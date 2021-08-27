f3448fdf91f2ba771b6f8df531bd5555d8b3207c
recherst <ygliusz@gmail.com>
2021-08-27 10:42:56 +0800

    Setup whole app structure

---
+++ SwipeableInstagramLayout/SwipeableInstagramLayout/SwipeableInstagramLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableInstagramLayoutApp.swift
+//  SwipeableInstagramLayout
+//
+//  Created by recherst on 2021/8/27.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableInstagramLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwipeableInstagramLayout/SwipeableInstagramLayout/View/Home.swift
@@ -0,0 +1,91 @@
+//
+//  Home.swift
+//  SwipeableInstagramLayout
+//
+//  Created by recherst on 2021/8/27.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var selectedTab = "house.fill"
+    // To update for dark mode
+    @Environment(\.colorScheme) var scheme
+
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    var body: some View {
+        // Instagram home view
+        TabView(selection: $selectedTab) {
+            Text("Home")
+                .tag("House.fill")
+            Text("Search")
+                .tag("magnifyingglass")
+            Text("Reals")
+                .tag("airplayvideo")
+            Text("Liked")
+                .tag("suit.heart.fill")
+            Text("Account")
+                .tag("person.circle")
+        }
+        .overlay(
+            // Custom tab bar
+            VStack(spacing: 12) {
+                Divider()
+                    .padding(.horizontal, -15)
+
+                HStack(spacing: 0) {
+                    TabBarButton(image: "house.fill", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+
+                    TabBarButton(image: "magnifyingglass", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+
+                    TabBarButton(image: "airplayvideo", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+
+                    TabBarButton(image: "suit.heart.fill", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+
+                    TabBarButton(image: "person.circle", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom, edges?.bottom ?? 15)
+            .background(scheme == .dark ? Color.black : Color.white)
+
+            , alignment: .bottom
+        )
+        .ignoresSafeArea()
+    }
+}
+
+// Tab bar button
+struct TabBarButton: View {
+    var image: String
+    @Binding var selectedTab: String
+
+    var body: some View {
+        Button(action: {
+            selectedTab = image
+        }, label: {
+            Image(systemName: image)
+                .font(.title2)
+                .foregroundColor(selectedTab == image ? .primary : .gray)
+
+        })
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+var rect = UIScreen.main.bounds
+var edges = UIApplication.shared.windows.first?.safeAreaInsets
