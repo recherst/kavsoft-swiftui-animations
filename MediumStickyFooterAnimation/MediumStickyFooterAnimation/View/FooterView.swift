f13d431f43168e22d6af958a1dc653ab7ac679b1
recherst <ygliusz@gmail.com>
2021-08-28 18:09:26 +0800

    Complete footer animation for contents scrolling

--- MediumStickyFooterAnimation/MediumStickyFooterAnimation/View/FooterView.swift
+++ MediumStickyFooterAnimation/MediumStickyFooterAnimation/View/FooterView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct FooterView: View {
+    // Dark mode
+    @State var darkMode = false
+    @State var offset: CGFloat = 0
+
     var body: some View {
         HStack {
             Button(action: {}, label: {
@@ -17,32 +21,62 @@ struct FooterView: View {
             })
             .frame(maxWidth: .infinity)
 
-            Button(action: {}, label: {
-                Image(systemName: "square.and.arrow.up")
-                    .font(.title2)
-                    .foregroundColor(.primary)
-            })
-            .frame(maxWidth: .infinity)
+            Group {
+                Button(action: {}, label: {
+                    Image(systemName: "square.and.arrow.up")
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                })
+                .frame(maxWidth: .infinity)
+                // Reduce distance while scrolling
+                .offset(x: offset == 0 ? 0 : 50 * (offset / 120))
 
-            Button(action: {}, label: {
-                Image(systemName: "bookmark")
-                    .font(.title2)
-                    .foregroundColor(.primary)
-            })
-            .frame(maxWidth: .infinity)
+                Button(action: {}, label: {
+                    Image(systemName: "bookmark")
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                })
+                .frame(maxWidth: .infinity)
 
-            Button(action: {}, label: {
-                Image(systemName: "moon")
-                    .font(.title2)
-                    .foregroundColor(.primary)
-            })
-            .frame(maxWidth: .infinity)
+                Button(action: {
+                    withAnimation { darkMode.toggle() }
+                }, label: {
+                    Image(systemName: darkMode ? "sun.min.fill" : "moon")
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                })
+                .frame(maxWidth: .infinity)
+            }
+            .offset(x: offset)
         }
         .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 15)
         .padding(.top)
         // Maxheight
         .frame(maxHeight: 80)
-        .background(Color.white)
+        .background(darkMode ? Color.black : Color.white)
+        .preferredColorScheme(darkMode ? .dark : .light)
+        .overlay(
+            // geometry reader for get maxY and animation
+            GeometryReader { reader -> Color in
+                let maxY = reader.frame(in: .global).maxY
+                let maxheight = UIScreen.main.bounds.height
+
+                DispatchQueue.main.async {
+                    if maxY < maxheight {
+                        if offset <= 120 {
+                            // Smoothly moving the offset to 120
+
+                            // up to -100 top scroll
+                            // It's your wish to animate
+                            let progress = (maxheight - maxY) / 100
+                            self.offset = progress * 120 <= 120 ? progress * 120 : 120
+                        }
+                    }
+                }
+                return Color.clear
+            }
+            , alignment: .bottom
+        )
 
     }
 }
