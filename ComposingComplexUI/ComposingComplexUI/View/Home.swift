dc38cc5148028257e6f756e43726d25d04f275ba
recherst <ygliusz@gmail.com>
2021-09-08 22:23:43 +0800

    Setup custom switch view

--- ComposingComplexUI/ComposingComplexUI/View/Home.swift
+++ ComposingComplexUI/ComposingComplexUI/View/Home.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct Home: View {
+
+    @State var selected = servers.first!
+
+    // Server toggle Status
+    @State var isServerOn = false
+
     var body: some View {
         VStack {
 
@@ -43,16 +49,129 @@ struct Home: View {
             }
             .padding()
 
-            Spacer()
+            ScrollView(.horizontal, showsIndicators: false, content: {
+
+                HStack(spacing: 15) {
+                    ForEach(servers) { server in
+                        Image(server.flag)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 45, height: 45)
+                            .clipShape(Circle())
+                            .onTapGesture {
+                                withAnimation {
+                                    selected = server
+                                }
+                            }
+                            .padding(5)
+                            .background(
+                                Circle()
+                                    .stroke(Color.white.opacity(selected.id == server.id ? 1 : 0), lineWidth: 1)
+                            )
+                    }
+                }
+                .padding()
+            })
+
+            ZStack {
+                // World map
+                Image("world")
+//                    .renderingMode(.template)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.width - 40)
+
+                VStack(spacing: 10) {
+                    if isServerOn {
+                        Text(selected.name)
+                            .fontWeight(.semibold)
+                    }
+
+                    Text(isServerOn ? "Connected" : "Not Connect")
+                        .fontWeight(.heavy)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+
+                    if isServerOn {
+                        HStack {
+                            Image(selected.flag)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 25, height: 25)
+                                .clipShape(Circle())
+
+                            Text("222.444.555.67")
+                                .fontWeight(.semibold)
+                        }
+                        .foregroundColor(.white)
+                    }
+                }
+                .offset(y: -20)
+            }
+            .padding(.vertical, 25)
+
+            // Toggle button
+            ZStack(alignment: Alignment(horizontal: .center, vertical: isServerOn ? .bottom : .top), content: {
+                Capsule()
+                    .fill(Color(isServerOn ? "dragbg1" : "dragbg2"))
+
+                VStack {
+                    if !isServerOn { Spacer() }
+
+                    Image(systemName: "chevron.\(isServerOn ? "up" : "down")")
+                        .font(.system(size: 28, weight: .bold))
+                        .foregroundColor(.white)
+
+                    Image(systemName: "chevron.\(isServerOn ? "up" : "down")")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(Color.white.opacity(0.7))
+
+                    if isServerOn { Spacer() }
+                }
+                .offset(y: isServerOn ? 30 : -30)
+
+                ZStack {
+                    Capsule()
+                        .fill(Color(isServerOn ? "drag2" : "drag1"))
+                        .frame(height: 180)
+                        .padding(10)
+
+                    VStack(spacing: 16) {
+                        Capsule()
+                            .fill(isServerOn ? Color.black.opacity(0.45) : Color.green)
+                            .frame(width: 30, height: 8)
+
+                        Text(isServerOn ? "STOP" : "START")
+                            .fontWeight(.heavy)
+                            .foregroundColor(.white)
+
+                        Image(systemName: "power")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Color.black.opacity(0.25))
+                            .clipShape(Circle())
+                    }
+                }
+                .onTapGesture {
+                    withAnimation {
+                        isServerOn.toggle()
+                    }
+                }
+            })
+            .frame(width: 130)
+            .padding(.bottom, 60)
         }
         .background(
             LinearGradient(
-                gradient: .init(colors: [Color("bg11"), Color("bg12")]),
+                gradient: .init(colors: [Color(isServerOn ? "bg11" : "bg21"), Color(isServerOn ? "bg12" : "bg22")]),
                 startPoint: .top,
                 endPoint: .bottom
             )
             .ignoresSafeArea()
+            .animation(.easeIn)
         )
+        .preferredColorScheme(.dark)
     }
 }
 
