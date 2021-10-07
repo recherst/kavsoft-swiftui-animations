abfe2de09c2555bc0934debe5bd61bd4ec8051ca
recherst <ygliusz@gmail.com>
2021-10-07 13:47:09 +0800

    Initialize project.

---
+++ StackingElements/StackingElements/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StackingElements
+//
+//  Created by recherst on 2021/10/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StackingElements/StackingElements/StackingElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackingElementsApp.swift
+//  StackingElements
+//
+//  Created by recherst on 2021/10/7.
+//
+
+import SwiftUI
+
+@main
+struct StackingElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
