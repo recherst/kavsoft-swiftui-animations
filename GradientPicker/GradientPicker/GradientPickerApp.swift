3789624ac89ce729216168a961b53c43b1a32875
recherst <ygliusz@gmail.com>
2021-10-10 21:06:28 +0800

    Initialize project.

---
+++ GradientPicker/GradientPicker/GradientPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientPickerApp.swift
+//  GradientPicker
+//
+//  Created by recherst on 2021/10/10.
+//
+
+import SwiftUI
+
+@main
+struct GradientPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
